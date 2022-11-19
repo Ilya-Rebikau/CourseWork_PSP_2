04e680026b4b5303bd460ef21b424470b13fb380
Ilya-Rebikov <ilya.rebikov.2000@mail.ru>
2022-11-19 18:55:58 +0300

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -10,4 +10,3 @@ start "Run CourseWork.ComputingAPI_2" /D"%~dp0src\CourseWork.ComputingAPI" dotne
 start "Run CourseWork.ComputingAPI_3" /D"%~dp0src\CourseWork.ComputingAPI" dotnet run --launch-profile "CourseWork.ComputingAPI" --urls "http://localhost:5004"
 TIMEOUT 5
 explorer "http://localhost:5000"
-pause
